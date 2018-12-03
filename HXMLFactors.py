b88847dfa0fcd2e2f07416527a541e53a13dbe4a
VFermat <vitorge@al.insper.edu.br>
2018-12-02 22:32:57 -0200

    workin on the factor code;

--- HXMLFactors.py
+++ HXMLFactors.py
@@ -10,7 +10,92 @@ import pandas as pd
 
 class HXLFactors(object):
     
-    def investment_factor(self, high_returns, low_returns):
+    def __init__(self, prices, assets, ROE, marketcap):
+        """
+        Initializes the class creating the initial pandas DataFrame to be used on the 
+        construction of the factors.
+        
+        :param prices: A pandas DataFrame with all securities prices throughout the time.
+        :param assets: A pandas DataFrame with all securities assets throughout the time.
+        :param ROE: A pandas DataFrame with all securities Return on Common Equity (ROE) throughout the time.
+        :param marketcap: A pandas DataFrame with all securities MarketCap throughout the time.
+        """        
+        
+        self.prices = prices
+        self.assets = assets
+        self.ROE = ROE
+        self.marketcap = marketcap
+        self.high_IA = ['BHIAHR', 'BHIAMR', 'BHIALR', 'SHIAHR', 'SHIAMR', 'SHIALR']
+        self.low_IA = ['BLIAHR', 'BLIAMR', 'BLIALR', 'SLIAHR', 'SLIAMR', 'SLIALR']
+        self.high_roe = ['BHIAHR', 'BMIAHR', 'BLIAHR', 'SHIAHR', 'SMIAHR', 'SLIAHR']
+        self.low_roe = ['BHIALR', 'BMIALR', 'BLIALR', 'SHIALR', 'SMIALR', 'SLIALR']
+        
+        stocks = pd.DataFrame(index=self.prices.index)
+        stocks["Prices"] = self.prices[self.prices.columns[0]]
+        stocks["Assets"] = self.assets[self.assets.columns[0]]
+        stocks["ROE"] = self.ROE[self.ROE.columns[0]]
+        stocks["marketcap"] = self.marketcap[self.marketcap.columns[0]]
+        stocks["I/A"] = 0
+        self.stocks = stocks
+        
+    def calculate_factor(self):
+        
+        self.factors = pd.DataFrame(index=["HXLInvestment", "HXLProfitability"])
+        
+        for month in range(1, self.prices.shape[1]):
+            new_stocks = pd.DataFrame(index=self.prices.index) 
+            new_stocks["Prices"] = self.prices[self.prices.columns[month]]
+            new_stocks["Assets"] = self.assets[self.assets.columns[month]]
+            new_stocks["ROE"] = self.ROE[self.ROE.columns[month]]
+            new_stocks["marketcap"] = self.marketcap[self.marketcap.columns[month]]
+            
+            if "june" in new_stocks.columns[month]:
+                new_stocks["I/A"] = self._get_IA(self.stocks["Assets"], new_stocks["Assets"])
+            else:
+                new_stocks["I/A"] = self.stocks["I/A"]
+                
+            new_stocks["returns"] = self._get_returns(self.stocks["Prices"], new_stocks["Prices"])
+            new_stocks = self.classify_stocks(new_stocks, self.prices.columns[month])
+            
+            portfolios = self._get_portfolios(new_stocks)
+            portfolios_return = pd.DataFrame(index=portfolios.keys, column=["prtflreturn"])
+            portfolios_return["prtflreturn"] = portfolios_return.apply(self._get_portfolio_return, args=(self.stocks, new_stocks))
+            high_IA = portfolios_return[portfolios_return.index in self.high_IA].prtflreturn
+            low_IA = portfolios_return[portfolios_return.index in self.low_IA].prtflreturn
+            high_ROE = portfolios_return[portfolios_return.index in self.high_ROE].prtflreturn
+            low_ROE = portfolios_return[portfolios_return.index in self.low_ROE].prtflreturn
+            HXLInvestment = self._factor(high_IA, low_IA)
+            HXLProfitability = self._factor(high_ROE, low_ROE)
+            self.factors[self.prices.columns[month]] = pd.Series([HXLInvestment, HXLProfitability])
+        
+    def classify_stocks(self, stocks, month):
+        """
+        Sort all stocks based on Size, I/A and ROE.
+        
+        :param stocks: A pandas DataFrame containing stock`s information.
+        :param month: Str. Checks if it`s June. Used to re-classify Size and I/A.
+        :return: A pandas DataFrame with stock`s information and classification.
+        """
+        
+        classified_stocks = stocks.copy()
+        
+        if "june" in month:
+            classified_stocks['sizemdn'] = np.median(classified_stocks['marketcap'])
+            classified_stocks['clssize'] = classified_stocks.apply(self._size_class, axis=1)
+            classified_stocks['I/A30%'] = np.percentile(classified_stocks['I/A'], 30)
+            classified_stocks['I/A70%'] = np.percentile(classified_stocks['I/A'], 70)
+            classified_stocks['clsI/A'] = classified_stocks.apply(self._IA_class, axis=1)
+        
+        
+        classified_stocks['ROE30%'] = np.percentile(classified_stocks['ROE'], 30)
+        classified_stocks['ROE70%'] = np.percentile(classified_stocks['ROE'], 70)
+        classified_stocks['clsROE'] = classified_stocks.apply(self._ROE_class, axis=1)
+        classified_stocks['stockcls'] = classified_stocks['clssize'] + classified_stocks['clsI/A'] + classified_stocks['clsROE']
+    
+        return classified_stocks    
+    
+    @staticmethod
+    def _factor(high_returns, low_returns):
         """
         Calculates the one factor based on the returns of specific portfolios.
         
@@ -18,12 +103,14 @@ class HXLFactors(object):
         :param low_returns: List, Array, or Series with the returns of each of the 6 Low portfolios
         :return: Float. The Investmen Factor for that month.
         """
+        
         average_high = np.mean(high_returns)
         average_low = np.mean(low_returns)
         factor = average_high - average_low
         return factor
     
-    def get_portfolios(self, stocks):
+    @staticmethod
+    def _get_portfolios(stocks):
         """
         Gets which stocks are in each portfolio based on its classification (i.e. SHIALR)
         
@@ -39,31 +126,15 @@ class HXLFactors(object):
         return portfolios
     
     @staticmethod
-    def _classify_stocks(stocks, month):
-        """
-        Sort all stocks based on Size, I/A and ROE.
+    def _get_returns(row, stocks, new_stocks):
+        stocks_used = stocks[stocks["stockcls"] == row.index]
+        new_stocks_used = new_stocks[new_stocks["stockcls"] == row.index]
+        new_stocks_used["price_difference"] = (new_stocks_used["prices"] - stocks_used["prices"])/stocks_used["prices"]
+        treturn = np.sum(new_stocks_used["price_difference"]*new_stocks_used["marketcap"])
+        tweight = np.sum(new_stocks_used["marketcap"])
+        portfolio_return = treturn/tweight
+        return portfolio_return
         
-        :param stocks: A pandas DataFrame containing stock`s information.
-        :param month: Str. Checks if it`s June. Used to re-classify Size and I/A.
-        :return: A pandas DataFrame with stock`s information and classification.
-        """
-        
-        classified_stocks = stocks.copy()
-        
-        if month == "June":
-            classified_stocks['sizemdn'] = np.median(classified_stocks['marketcap'])
-            classified_stocks['clssize'] = classified_stocks.apply(_size_class, axis=1)
-            classified_stocks['I/A30%'] = np.percentile(classified_stocks['I/A'], 30)
-            classified_stocks['I/A70%'] = np.percentile(classified_stocks['I/A'], 70)
-            classified_stocks['clsI/A'] = classified_stocks.apply(_IA_class, axis=1)
-        
-        
-        classified_stocks['ROE30%'] = np.percentile(classified_stocks['ROE'], 30)
-        classified_stocks['ROE70%'] = np.percentile(classified_stocks['ROE'], 70)
-        classified_stocks['clsROE'] = classified_stocks.apply(_ROE_class, axis=1)
-        classified_stocks['stockcls'] = classified_stocks['clssize'] + classified_stocks['clsI/A'] + classified_stocks['clsROE']
-    
-        return classified_stocks
     
     @staticmethod
     def _size_class(row):
